0e1584aa3e0b432d3fa1dad4c396089c300a8022
LeurisCastillo <leurisa.delossantos@gmail.com>
2022-10-02 14:41:40 -0400

    feat: CompanyController added

--- WebApiDemo/Controllers/CompanyController.cs
+++ WebApiDemo/Controllers/CompanyController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApiDemo.Models;
+using WebApiDemo.Models.DTO.CompanyDTO;
 using WebApiDemo.Services.Interfaces;
 
 namespace WebApiDemo.Controllers
@@ -15,6 +18,105 @@ namespace WebApiDemo.Controllers
             this.service = service;
         }
 
-        
+        [HttpGet]
+        public IActionResult GetAllCompanies()
+        {
+            List<Company> companies;
+            try
+            {
+                companies = service.GetAll();
+                if (companies == null) return NotFound();
+                return Ok(companies);
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetCompanyById(int id)
+        {
+            Company company;
+            try
+            {
+                company = service.Get(id);
+                if (company == null) return NotFound();
+                return Ok(company);
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateCompany(CreateCompanyDto company)
+        {
+            try
+            {
+                Company output = new Company
+                {
+                    Name = company.Name
+                };
+
+                service.Create(output);
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCompany(UpdateCompanyDto company)
+        {
+            try
+            {
+                var temp = service.Get(company.Id);
+                if (temp == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    Company output = new Company
+                    {
+                        Id = company.Id,
+                        Name = company.Name,
+                    };
+
+                    service.Update(output);
+                    return Ok();
+                }
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("id")]
+        public IActionResult DeleteCompany(int id)
+        {
+            try
+            {
+                var company = service.Get(id);
+                if (company == null) 
+                {
+                    return NotFound();
+                } 
+                else
+                {
+                    service.Delete(id);
+                    return Ok();
+                }
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
